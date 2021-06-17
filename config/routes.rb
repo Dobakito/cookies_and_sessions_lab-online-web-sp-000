63289cc35d8af8ee5156251d2a34cf7deae54551
Dobakito <mdobak44@icloud.com>
2021-06-17 13:10:23 -0500

    commit tree 682f5bd65ee1d985e6eb36cb64fcf6872208f6c5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :cart
+  resources :product
+  get "/", to: 'products#index' 
 end
